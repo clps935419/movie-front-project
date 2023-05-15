5ce218c78ba30c9233d021098d78e10237cd22b4
cavalown <cavalown@gmail.com>
2023-05-16 03:06:45 +0800

    refactor: change movie ifon a independent page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,9 @@ import NotFound from '@/pages/NotFound';
 import Test from '@/pages/Test';
 import { Navigate } from 'react-router-dom';
 import BookTicket from '../pages/BookTicket';
-import MovieInfo from '../pages/BookTicket/MovieInfo';
 import TicketConfirm from '../pages/BookTicket/TicketConfirm';
 import TicketSeats from '../pages/BookTicket/TicketSeats';
+import MovieInfo from '../pages/MovieInfo';
 import MoviesList from '../pages/MoviesList';
 
 const routeConfig = [
@@ -45,15 +45,15 @@ const routeConfig = [
         element: <MovieInfo />,
       },
       {
-        path: "/ticket/:sessionId",
+        path: "/ticket/sessionId",
         element: <BookTicket />,
       },
       {
-        path: "/ticket/:sessionId/seats",
+        path: "/ticket/sessionId/seats",
         element: <TicketSeats />,
       },
       {
-        path: "/ticket/:sessionId/confirm",
+        path: "/ticket/sessionId/confirm",
         element: <TicketConfirm />,
       },
       //攔截器範例
