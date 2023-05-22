5128444ae37559637331ce8bba6b18584213edb0
kai <shanecg8v@gmail.com>
2023-05-22 23:43:38 +0800

    feat: add member header img

--- src/pages/Member/index.js
+++ src/pages/Member/index.js
@@ -1,6 +1,16 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 import '@/assets/scss/member/navTab.scss';
+import styled from 'styled-components';
+
+const HeaderImg = styled.div`
+  height: 287px;
+  width: 100%;
+  margin-bottom: 120px;
+  background: url('https://s3-alpha-sig.figma.com/img/0746/7826/d66997a7eb0caad9ab19c2dc72a69623?Expires=1685923200&Signature=ICakUf79-8EeW-jtcoF4SmXhycSWmPV3F1vY-pUoXJg9s1UCtcmhK08fRfhX0Kj7Dd-Nt2BilCv7ZaoZ3e~RCWipEsFfHFOx84ZYEUXlHM8Fu01DXgbSp0cO~OSpDcs-81jY5T~cCcn7M9z6hY4PfUJUAKmdT5TDyFOi1lit8OMiLNecb4XBvPJ2uCYbELekjDQQRlMpPbOu0pJSiranfGK1kNCJg-uYlTOFpB9TPhuqxIVVLWSxckTEIjYs0QoA4vMgKXMG7kvKx3c0tPgpk6Wx2HfhVFQ558Kx9VGwdNClk3OJF3oOPFEulap~LVVY1FdbM8zIsc9XByd7s-42mg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')
+    no-repeat center;
+  background-size: cover;
+`;
 
 const navItem = [
   { name: '個人資料', href: '#/member' },
@@ -11,19 +21,22 @@ const navItem = [
 const Member = () => {
   const location = useLocation();
   return (
-    <div className="container">
-      <Nav variant="tabs" className="member">
-        {navItem.map((item) => (
-          <Nav.Item key={item.name}>
-            <Nav.Link href={item.href} className={`${item.href === `#${location.pathname}` ? 'active' : ''}`}>
-              {item.name}
-            </Nav.Link>
-          </Nav.Item>
-        ))}
-      </Nav>
+    <>
+      <HeaderImg />
+      <div className="container">
+        <Nav variant="tabs" className="member">
+          {navItem.map((item) => (
+            <Nav.Item key={item.name}>
+              <Nav.Link href={item.href} className={`${item.href === `#${location.pathname}` ? 'active' : ''}`}>
+                {item.name}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+        </Nav>
 
-      <Outlet />
-    </div>
+        <Outlet />
+      </div>
+    </>
   );
 };
 
