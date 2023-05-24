bda8b1af9f347e137a57ad734c7dbfdcbafa7694
kai <shanecg8v@gmail.com>
2023-05-25 01:12:21 +0800

    feat: 新增影廳頁面

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,12 @@
 import * as user from '@/api/user';
-import * as activity from "@/api/activity";
-import * as movies from "@/api/movies";
-import * as home from "@/api/home";
+
+import * as activity from '@/api/activity';
+import * as movies from '@/api/movies';
+import * as home from '@/api/home';
+import * as theater from '@/api/theater';
 
 export const apiUser = user;
 export const apiActivity = activity;
 export const apiMovies = movies;
 export const apiHome = home;
+export const apiTheater = theater;
---
+++ src/pages/TheaterList/components/TheaterCard.js
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const TheaterCard = ({ theater }) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={theater.imgUrl} />
+      <Card.Body>
+        <Card.Title>{theater.name}</Card.Title>
+        <p className="mb-0">{theater.address}</p>
+        <p>{theater.phone}</p>
+        <Button className="float-end" variant="customBtn1" href={`#/theaters/${theater.name}`}>
+          詳細資訊
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default TheaterCard;
