6d8bd0eac93a4b7dbdff5dcf71567279f3308e92
kai <shanecg8v@gmail.com>
2023-06-18 23:14:39 +0800

    modfiy: 修改紅利

--- src/pages/Member/BonusRecord.js
+++ src/pages/Member/BonusRecord.js
@@ -34,18 +34,22 @@ const BonusRecord = () => {
   }
 
   return (
-    <div className="row gap-5 mb-5">
-      <div className="border border-2 rounded text-center col-3 offset-1 h-100 d-flex flex-column justify-content-center py-5">
-        <h3>會員點數</h3>
-        <p>
-          共<span className="h3 px-2 text-primary">{bonusData.bonus}</span>點
-        </p>
-        <p className="mb-0">
-          其中{bonusData.expire.bonus}點將於{bonusData.expire.date}過期
-        </p>
+    <div className="row gap-md-3 gap-2 mb-5">
+      <div className="col-12 col-md-3 offset-md-1 h-100 ">
+        <div className="border border-2 rounded text-center py-5 px-md-5">
+          <h3>會員點數</h3>
+          <p>
+            共<span className="h3 px-2 text-primary">{bonusData.bonus}</span>點
+          </p>
+          <p className="mb-0">
+            其中{bonusData.expire.bonus}點將於{bonusData.expire.date}過期
+          </p>
+        </div>
       </div>
-      <div className="border border-2 rounded text-center col-7 h-100 d-flex flex-column justify-content-center pt-2">
-        {orderDetail}
+      <div className="col-12 col-md-7 h-100 ">
+        <div className="border border-2 rounded text-center d-flex flex-column justify-content-center pt-2">
+          {orderDetail}
+        </div>
       </div>
     </div>
   );
